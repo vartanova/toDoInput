03f87bec2c6440c5c4c47a78d616dc875c0d8b53
vartanova <vartanova.fortech@gmail.com>
2025-09-21 22:52:21 +0300

    linked dropdown to input

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-
 let dropDown = document.getElementById('dropDown')
 let inputText = document.getElementById('inputText')
 let saveBtn = document.getElementById('saveText')
 let clearBtn = document.getElementById('clearText')
+let selectOption = document.getElementById("idOption")
+
+function clearText () {
+    inputText.value = ""
+}
 
 function saveInputValue (value) {
     if (inputText.value != "") {
@@ -18,15 +22,19 @@ function saveInputValue (value) {
             newElem.value = value
             newElem.textContent = value
             dropDown.prepend(newElem)
-        } else {inputText.value = 'Вы уже это вводили'
-        inputText.style.color = 'red'
-        }
+            newElem.id = 'idOption'
+
+        } else {inputText.value = 'Вы уже это вводили'}
     }
 }
   
-
-function clearText () {
-    inputText.value = ""
+function selectValue () {
+    let indexOption = dropDown.selectedIndex
+    if (indexOption !== -1) {
+        inputText.value = dropDown.options[indexOption].value
+    }
 }
-clearBtn.onclick = clearText
+
 saveBtn.onclick = function () {saveInputValue(inputText.value)}
+dropDown.onchange = selectValue
+clearBtn.onclick = clearText
