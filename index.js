58dcedda6b6c334e45ec98c0dd3d92e245277ab1
vartanova <vartanova.fortech@gmail.com>
2025-09-23 23:42:15 +0300

    fix bug

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
-let dropDown = document.getElementById('dropDown')
-let inputText = document.getElementById('inputText')
-let saveBtn = document.getElementById('saveText')
-let clearBtn = document.getElementById('clearText')
-let selectOption = document.getElementById("idOption")
-let deleteBtn = document.getElementById('deleteText')
-let changeBtn = document.getElementById('changeText')
+const dropDown = document.getElementById('dropDown')
+const inputText = document.getElementById('inputText')
+const saveBtn = document.getElementById('saveText')
+const clearBtn = document.getElementById('clearText')
+const selectOption = document.getElementById("idOption")
+const deleteBtn = document.getElementById('deleteText')
+const changeBtn = document.getElementById('changeText')
 
 
 function clearText () {
     inputText.value = ""
 }
 
-function saveInputValue (value) {
-    if (inputText.value !== "") {
-        let flag = false
-        for (let i = 0; i < dropDown.options.length; i++) {
-            if (dropDown.options[i].value === value) {
-                flag = true
-                break
-            }
-        }
-        if (!flag) {
-            let newElem = document.createElement('option')
-            newElem.value = value
-            newElem.textContent = value
-            dropDown.prepend(newElem)
-            newElem.id = 'idOption'
-            
+const listsFilm = ['Harry Potter', 'Charmed']
 
-        } else {inputText.value = 'Вы уже это вводили'}
+function addFilm (film) { //вынесла в отдельную функцию создание нового фильма
+    const option = document.createElement('option')
+    option.value = film
+    option.textContent = film
+    dropDown.appendChild(option)
+    dropDown.value = film // чтоб при создании элемента в дропдауне автоматически выбирался новый созданный фильм
+    inputText.value = option.value // меняю значение в инпуте на новое
+}
+listsFilm.forEach(addFilm) //добавляю в дропдаун все элементы списка
+
+function addNewFilm () {
+    let currInput = inputText.value // значение инпута
+    if (currInput === '') { //если инпут пустой
+        alert('Введите название фильма')
+        return
     }
-    for (let i = 0; i < dropDown.options.length; i++) {
-        if (dropDown.options[i].value === 'Вы уже это вводили') {
-            dropDown.remove(i)
-            inputText.value = ""
-        }
+    let filmIncludes = listsFilm.includes(currInput) //вынесла в отдельную переменную проверку значения инпута в списке фильмов
+    if (!filmIncludes) { //если введенного фильма не нашлось, то добавляю новый
+        listsFilm.push(currInput)
+        addFilm(currInput)
+        dropDown.value = currInput // меняю значение дропдауна на новый созданный фильм
+    } else { //если такой фильм нашелся в списке, то 
+        alert('Вы это уже вводили')
     }
 }
   
 function selectValue () {
     let indexOption = dropDown.selectedIndex
     if (indexOption !== -1) {
-        inputText.value = dropDown.options[indexOption].value
-    }
+        inputText.value = dropDown.options[indexOption].value //здесь соединяю инпут с конкретным элементом в дропдауне и применяю на кнопку onchange,
+    } //чтоб когда мы изменяли выбор в дропдауне, изменялся текст и в инпуте
 }
 function deleteValue () {
-    let inputValue = inputText.value
+    const dropDownElemSelected = dropDown.value
+    if (!dropDownElemSelected) {
+        alert('Выберите фильм для удаления')
+    }
 
-    for (let i = 0; i < dropDown.options.length; i++) {
-        if (dropDown.options[i].value === inputValue) {
+    for (let i = 0; i < listsFilm.length; i++) {
+        if (dropDown.options[i].value === dropDownElemSelected) {
             dropDown.remove(i)
             inputText.value = ""
         }
@@ -63,7 +67,7 @@ inputText.onfocus = function () {
 
 function changeValue () {
     let currInputValue = inputText.value //здесь у нас новое значение
-    for (let i = 0; i < dropDown.options.length; i++) {
+    for (let i = 0; i < listsFilm.length; i++) {
         if (dropDown.options[i].value === oldInputValue ) { //смотрю, если такой элемент уже существует, то его обновляю
             dropDown.options[i].value = currInputValue
             dropDown.options[i].textContent = currInputValue
@@ -73,8 +77,9 @@ function changeValue () {
 }
 
 
-saveBtn.onclick = function () {saveInputValue(inputText.value)}
+
 dropDown.onchange = selectValue
-clearBtn.onclick = clearText
-deleteBtn.onclick = deleteValue
-changeBtn.onclick = changeValue
+saveBtn.addEventListener('click', addNewFilm)
+clearBtn.addEventListener('click', clearText)
+deleteBtn.addEventListener('click', deleteValue)
+changeBtn.addEventListener('click', changeValue)
